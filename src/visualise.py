4e1b1a4d8f3eded3d0e9e5fd2b8bc7e0b150013b
Frederik Gantriis Møller <frmo24042@gmail.com>
2025-11-20 14:55:55 +0100

    feat(visualise): add visualisation step

---
+++ src/visualise.py
@@ -0,0 +1,78 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from dvclive.live import Live
+
+
+def get_waveform(filename, y, sr):
+    # ---- 1. Waveform ----
+    plt.figure(figsize=(12, 4))
+    librosa.display.waveshow(y, sr=sr)
+    plt.title(f"Waveform {filename}")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Amplitude")
+    plt.tight_layout()
+
+    return plt.gcf()
+
+
+def get_spectrogram(filename, y, sr):
+    # ---- 2. Spectrogram ----
+    D = np.abs(librosa.stft(y))
+    DB = librosa.amplitude_to_db(D, ref=np.max)
+
+    plt.figure(figsize=(12, 4))
+    librosa.display.specshow(DB, sr=sr, x_axis="time", y_axis="log")
+    plt.colorbar(format="%+2.0f dB")
+    plt.title(f"Spectrogram (STFT) {filename}")
+    plt.tight_layout()
+
+    return plt.gcf()
+
+
+def get_mel_spectrogram(filename, y, sr):
+    # ---- 3. Mel-Spectrogram ----
+    mel = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=64)
+    mel_db = librosa.power_to_db(mel, ref=np.max)
+
+    plt.figure(figsize=(12, 4))
+    librosa.display.specshow(mel_db, sr=sr, x_axis="time", y_axis="mel")
+    plt.colorbar(format="%+2.0f dB")
+    plt.title(f"Mel-Spectrogram {filename}")
+    plt.tight_layout()
+
+    return plt.gcf()
+
+
+def main():
+    base_url = os.path.join("raw-data", "genres_original")
+
+    filenames = [
+        os.path.join(base_url, "classical", "classical.00000.wav"),
+        os.path.join(base_url, "blues", "blues.00000.wav"),
+        os.path.join(base_url, "rock", "rock.00000.wav"),
+        os.path.join(base_url, "pop", "pop.00000.wav"),
+        os.path.join(base_url, "jazz", "jazz.00000.wav"),
+        os.path.join(base_url, "country", "country.00000.wav"),
+        os.path.join(base_url, "disco", "disco.00000.wav"),
+        os.path.join(base_url, "hiphop", "hiphop.00000.wav"),
+        os.path.join(base_url, "metal", "metal.00000.wav"),
+        os.path.join(base_url, "reggae", "reggae.00000.wav"),
+    ]
+
+    with Live("visualise") as live:
+        for filename in filenames:
+            y, sr = librosa.load(filename, sr=None)
+
+            live.log_image(f"{filename}-waveform.png",
+                           get_waveform(filename, y, sr))
+            live.log_image(f"{filename}-spectrogram.png",
+                           get_spectrogram(filename, y, sr))
+            live.log_image(f"{filename}-mel_spectrogram.png",
+                           get_mel_spectrogram(filename, y, sr))
+
+
+if __name__ == "__main__":
+    main()
